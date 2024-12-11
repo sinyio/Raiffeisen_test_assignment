5ef244f52907f9fd29ecbf30614a23f4e3a54cd4
Ilya Sinyakin <arqu1t@mail.ru>
2024-12-12 00:51:33 +0300

    gap fix

--- src/modules/CountryList/ui/styled.ts
+++ src/modules/CountryList/ui/styled.ts
@@ -4,25 +4,24 @@ export const Header = styled.div`
   display: flex;
   padding-inline: 24px;
   font-size: 24px;
-  gap: 16px;
+  gap: 24px;
 
   @media (width < 1025px) {
     font-size: 16px;
-    gap: 12px;
+    gap: 16px;
   }
 
   @media (width < 769px) {
     padding-inline: 16px;
     font-size: 14px;
-    gap: 8px;
+    gap: 12px;
   }
 `;
 
 export const HeaderColumn = styled.p`
-  color: #000;
   flex: 1;
 
-  &:nth-child(1) {
+  &:first-child {
     flex: 0.4;
   }
 `;
