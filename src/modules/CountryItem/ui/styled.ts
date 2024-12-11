954e35af4f0e29cc9f398412d109d919dfdca372
Ilya Sinyakin <arqu1t@mail.ru>
2024-12-11 04:17:12 +0300

    style fix

--- src/modules/CountryItem/ui/styled.ts
+++ src/modules/CountryItem/ui/styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const CountryItemContainer = styled.div`
   display: flex;
   align-items: center;
-  gap: 16px;
+  gap: 24px;
   height: 100px;
   padding: 8px 24px;
   border: 1px solid grey;
@@ -14,14 +14,14 @@ export const CountryItemContainer = styled.div`
   @media (width < 1025px) {
     height: 80px;
     font-size: 16px;
-    gap: 12px;
+    gap: 16px;
   }
 
   @media (width < 769px) {
     height: 70px;
     padding: 8px 16px;
     font-size: 14px;
-    gap: 8px;
+    gap: 12px;
   }
 `;
 
